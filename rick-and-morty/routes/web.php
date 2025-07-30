0a7457c34ef5805fef44f961672e83733f0a3fe6
gmgpx <gugapires92@gmail.com>
2025-07-29 23:18:01 -0300

    fix: default view routing and post-login fix

--- rick-and-morty/routes/web.php
+++ rick-and-morty/routes/web.php
@@ -2,11 +2,20 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CharacterController;
 
-Route::get('/', [CharacterController::class, 'index'])->name('home');
-Route::get('/characters', [CharacterController::class, 'myCharacters'])->name('characters.index');
-Route::post('/characters', [CharacterController::class, 'store'])->name('characters.store');
-Route::get('/characters/{id}', [CharacterController::class, 'show'])->name('characters.show');
-Route::delete('/characters/{id}', [CharacterController::class, 'destroy'])->name('characters.destroy');
-Route::get('/about', fn() => view('about'))->name('about');
+Route::get('/home', function () {
+    return redirect()->route('home');
+});
 
+Route::get('/', [CharacterController::class, 'index'])->name('home');
+Route::get('/characters/{id}', [CharacterController::class, 'show'])->name('characters.show');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/characters', [CharacterController::class, 'myCharacters'])->name('characters.index');
+    Route::post('/characters', [CharacterController::class, 'store'])->name('characters.store');
+    Route::delete('/characters/{id}', [CharacterController::class, 'destroy'])->name('characters.destroy');
+    Route::get('/characters/{id}/edit', [CharacterController::class, 'edit'])->name('characters.edit');
+    Route::put('/characters/{id}', [CharacterController::class, 'update'])->name('characters.update');
+});
+
+Route::get('/about', fn() => view('about'))->name('about');
 Auth::routes();
